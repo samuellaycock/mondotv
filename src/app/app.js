7aacb3d618e46dc9dee7022807cc4b80fb5525db
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-08-30 19:51:02 +0100

    Build error.

--- src/app/app.js
+++ src/app/app.js
@@ -1,4 +1,4 @@
-angular.module('app', ['ui.router', 'ngAnimate', 'angularUtils.directives.dirPagination']);
+angular.module('app', ['ui.router', 'ngAnimate', 'angularUtils.directives.dirPagination', 'slick']);
 
 /**
  *
