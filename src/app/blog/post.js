1d36e7ad53d8d47a9dde50b0c03244364b0760cf
Samuel Laycock <samuel.paul.laycock@gmail.com>
2016-09-01 13:13:04 +0100

    Update single post view.

--- src/app/blog/post.js
+++ src/app/blog/post.js
@@ -1,7 +1,12 @@
 function PostController($stateParams, $anchorScroll, $timeout, $location, PostService, MetadataService) {
-    var vm = this;
+    var vm = this,
+        el = document.getElementsByTagName('form');
 
     vm.post = {};
+
+    if(el) {
+      el.className += el.className ? ' pure-form pure-form-aligned' : 'pure-form pure-form-aligned';
+    }
 
     PostService.singlePostById($stateParams.id).then(function(post) {
         vm.post = post;
