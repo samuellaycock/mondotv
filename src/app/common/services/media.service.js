351082f3919251e75bbda1b2ffd7c6b5a05faca6
root <root@ip-172-31-45-137.us-west-2.compute.internal>
2016-09-12 13:07:02 +0000

    Update services.

--- src/app/common/services/media.service.js
+++ src/app/common/services/media.service.js
@@ -8,33 +8,34 @@
  */
 function MediaService(DataService) {
 
-    // Search and return all media
-    function allMedia() {
-        return DataService.getData('media');
+  // Search and return all media
+  function allMedia() {
+      return DataService.getData('media');
+  }
+
+  // Search and return single media object by ID
+  function singleMediaById(id) {
+      return DataService.getData('media/' + id);
+  }
+
+  function decorateObjectWithMedia(object) {
+    if (object.featured_media > 0) {
+      var imageId = object.featured_media;
+
+      singleMediaById(imageId).then(function(imageData) {
+        object.featured_media = imageData.media_details;
+      });
     }
+    return object;
+  }
 
-    // Search and return single media object by ID
-    function singleMediaById(id) {
-        return DataService.getData('media/' + id);
-    }
-
-    function decorateObjectWithMedia(result) {
-      if (result.featured_media > 0) {
-        var imageId = result.featured_media,
-            imageData = singleMediaById(imageId);
-
-        result.images = imageData.$$state;
-      }
-      return result;
-    }
-
-    return {
-        allMedia: allMedia,
-        singleMediaById: singleMediaById,
-        decorateObjectWithMedia: decorateObjectWithMedia
-    };
+  return {
+    allMedia: allMedia,
+    singleMediaById: singleMediaById,
+    decorateObjectWithMedia: decorateObjectWithMedia
+  };
 }
 
 angular
-    .module('app')
-    .factory('MediaService', MediaService);
+  .module('app')
+  .factory('MediaService', MediaService);
